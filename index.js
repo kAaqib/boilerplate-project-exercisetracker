dda2acd1fcf785520f62ca6551c4f14bced79346
kAaqib <124460797+kAaqib@users.noreply.github.com>
2024-06-16 16:49:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,16 +3,18 @@ const app = express()
 const cors = require('cors')
 const mon = require('mongoose');
 const bodyParser = require('body-parser');
+require('node:util')
 require('dotenv').config()
 
 mon.connect("mongodb+srv://fccdb:fccdb@cluster0.teunbos.mongodb.net/fccdb?retryWrites=true&w=majority&appName=Cluster0", { useNewUrlParser: true, useUnifiedTopology: true });
 const exeSchema = mon.Schema({
   username: String,
+  count: {type: Number, default: 0},
   log: [{
     description: String,
     duration: String,
     date: Date
-  }]
+  }],
 });
 
 const exeModel = mon.model("exeModel", exeSchema);
@@ -30,14 +32,65 @@ app.post("/api/users", function(req, res) {
     username: uname
   })
   user.save()
-  exeModel.findOne({username: uname})
-    .then(function(data) {
-      const id = data._id;
-      res.json({username: uname, _id: id})
+  if (uname != "") {
+    exeModel.findOne({username: uname})
+      .then(function(data) {
+        const id = data._id;
+        //res.send(data);
+        res.json({username: uname, _id: id})
+      })
+    }
+});
+
+app.get("/api/users", function(req, res) {
+  exeModel.find()
+    .then(function(users) {
+      var userMap = [];
+      users.forEach(function(user) {
+        userMap.push = user;
+      });
+      res.send(users);
     })
 });
 
+app.post("/api/users/:_id/exercises", function(req, res) {
+  const id = req.params._id;
+  const desc = req.body.description;
+  const dur = req.body.duration;
+  const date = req.body.date ? new Date(req.body.date) : new Date();
+  const d = new Date(date);
+  const exe = {description: desc, duration: dur, date: date}
+  exeModel.findById({_id: id})
+    .then(function(user) {
+      user.log.push(exe);
+      user.count++;
+      user.save()
+      .then(function(updatedUser) {
+        const formattedUser = updatedUser.toObject();
+        formattedUser.log = formattedUser.log.map(entry => ({
+          description: entry.description,
+          duration: entry.duration,
+          date: entry.date.toDateString() // format the date
+        }));
+      })
+      res.json({_id: id, username: user.username, date: d.toDateString(), duration: dur, description: desc});
+    })
+});
 
+app.get("/api/users/:_id/logs", function(req, res) {
+  const id = req.params._id;
+  exeModel.findById({_id: id})
+    .select({__v: 0})
+    .then(function(user) {
+      const formattedUser = user.toObject();
+      formattedUser.log = formattedUser.log.map(entry => ({
+        description: entry.description,
+        duration: Number(entry.duration),
+        date: entry.date.toDateString() // format the date
+      }));
+      res.status(200).send(formattedUser);
+    });
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
